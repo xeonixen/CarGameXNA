226e0dc7621a72495d3224721651975d035fbb44
Marcus Lönneborg <marcuslonneborg@gmail.com>
2015-09-25 22:19:51 +0200

    added torque effect on car body added scrolling background

--- CarGameXNA/CarGameXNA/Game1.cs
+++ CarGameXNA/CarGameXNA/Game1.cs
@@ -17,22 +17,28 @@ namespace CarGameXNA
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        float tempdivider = 0;
+        float groundY = 753f;
+        float velY = 0f;
+
         int _total_frames = 0;
         float _elapsed_time = 0.0f;
         int _fps = 0;
-        bool prevKeyGearUp, prevKeyGearDown;
+        bool prevKeyGearUp, prevKeyGearDown, prevKeyReverse;
         float power = 0;
-        double powerUpdateTime=0;
+        double powerUpdateTime = 0;
         SpriteFont font;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Car playerCar;
         Texture2D playerCarTexture;
         Texture2D playerCarWheel;
-        float wheelRotation = 0f;
+        Scrolling scrolling1, scrolling2;
+        float wheelRotation = 0f, playerRotation = 0f;
         Vector2 wheelPivot;
-        Vector2 playerPosition = new Vector2(0f, 600f);
-        Vector2 wheelPosition = new Vector2(0f, 600f);
+        Vector2 playerPivot;
+        Vector2 playerPosition = new Vector2(100f, 1f);
+        Vector2 wheelPosition = new Vector2(100f, 1f);
 
         public Game1()
         {
@@ -70,6 +76,10 @@ namespace CarGameXNA
             font = Content.Load<SpriteFont>("XNAFont");
             playerCarWheel = Content.Load<Texture2D>("wheel");
             wheelPivot = new Vector2(playerCarWheel.Width / 2, playerCarWheel.Height / 2);
+            playerPivot = new Vector2(playerCarTexture.Width / 2, playerCarTexture.Height / 2);
+            scrolling1 = new Scrolling(Content.Load<Texture2D>("background"), new Vector2(0, 0));
+            scrolling2 = new Scrolling(Content.Load<Texture2D>("background"), new Vector2(scrolling1.texture.Width, 0));
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -90,7 +100,7 @@ namespace CarGameXNA
         protected override void Update(GameTime gameTime)
         {
             _elapsed_time += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            if (_elapsed_time  >= 1000.0f)
+            if (_elapsed_time >= 1000.0f)
             {
                 _fps = _total_frames;
                 _total_frames = 0;
@@ -132,9 +142,17 @@ namespace CarGameXNA
             else
                 playerCar.brakePosition -= 5;
 
-          
-
-
+            if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.R))
+            {
+                playerCar.GearReverse(true);
+                prevKeyReverse = true;
+            }
+            else
+            {
+                if (prevKeyReverse)
+                    prevKeyReverse = false;
+            }
+            
             if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.Escape))
                 this.Exit();
 
@@ -143,10 +161,26 @@ namespace CarGameXNA
             playerCar.Calculate(gameTime.ElapsedGameTime.TotalMilliseconds);
 
 #warning "Trial and error to find out values to divide speed with"
-            playerPosition.X += (float)playerCar.GetSpeed() / 5f;
-            wheelPosition = playerPosition + new Vector2(45, 85);
-            wheelRotation += (float)playerCar.GetSpeed() / 200f;
-              
+            // playerPosition.X += (float)playerCar.GetSpeed() / 7f;
+            wheelPosition = playerPosition + new Vector2(45, 82);
+            wheelRotation += (float)playerCar.GetSpeed() / 140f;
+
+            if ((wheelPosition.Y + playerCarWheel.Height / 2 < groundY))
+                velY += 9.81f / 1000f;
+            else
+            {
+                velY = 0;
+                playerPosition.Y = groundY - 82;
+            }
+
+            tempdivider = (float)(gameTime.ElapsedGameTime.TotalMilliseconds / 1000d);
+            if (tempdivider != 0) playerPosition.Y += velY / tempdivider;
+
+
+            playerRotation = -(float)(playerCar.getForceOnCarX(gameTime.ElapsedGameTime.TotalMilliseconds) / (gameTime.ElapsedGameTime.TotalMilliseconds * 200f));
+
+
+
             if (playerPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - playerCarTexture.Width)
                 playerPosition.X = 0;
             if (gameTime.TotalGameTime.TotalSeconds - powerUpdateTime > 0.1d)
@@ -154,8 +188,27 @@ namespace CarGameXNA
                 powerUpdateTime = gameTime.TotalGameTime.TotalSeconds;
                 power = (power + playerCar.engine.GetPowerHP()) / 2f;
             }
-                // TODO: Add your update logic here
 
+            // TODO: Add your update logic here
+            if (playerCar.GetSpeed() > 0)
+            {
+                if (scrolling1.vector.X + scrolling1.texture.Width <= 0)
+                    scrolling1.vector.X = scrolling2.vector.X + scrolling2.texture.Width;
+                if (scrolling2.vector.X + scrolling2.texture.Width <= 0)
+                    scrolling2.vector.X = scrolling1.vector.X + scrolling1.texture.Width;
+            }
+            else
+            {
+                if (scrolling1.vector.X >= GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width)
+                    scrolling1.vector.X = scrolling2.vector.X - scrolling1.texture.Width;
+                if (scrolling2.vector.X >= GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width)
+                    scrolling2.vector.X = scrolling1.vector.X - scrolling2.texture.Width;
+            }
+
+
+
+            scrolling1.Update(-(float)playerCar.GetSpeed() / 7f);
+            scrolling2.Update(-(float)playerCar.GetSpeed() / 7f);
             base.Update(gameTime);
         }
 
@@ -166,14 +219,17 @@ namespace CarGameXNA
         protected override void Draw(GameTime gameTime)
         {
             _total_frames++;
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightGreen);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(playerCarTexture, playerPosition, Color.White);
-            spriteBatch.Draw(playerCarWheel, wheelPosition ,null, Color.White,wheelRotation,wheelPivot,1.0f,SpriteEffects.None,0f);
-            spriteBatch.Draw(playerCarWheel, wheelPosition+new Vector2(120,0), null, Color.White, wheelRotation, wheelPivot, 1.0f, SpriteEffects.None, 0f);
-            spriteBatch.DrawString(font, String.Format("Speed: {0}\nRpm: {1}\nGear: {2}\nPower(Hp): {3}\nFps: {4}", playerCar.GetSpeed(),playerCar.engine.currentRPM, playerCar.Gear,(int)power,_fps), Vector2.Zero, Color.Black);
+            scrolling1.Draw(spriteBatch);
+            scrolling2.Draw(spriteBatch);
+            spriteBatch.Draw(playerCarWheel, wheelPosition, null, Color.White, wheelRotation, wheelPivot, 1.0f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(playerCarWheel, wheelPosition + new Vector2(120, 0), null, Color.White, wheelRotation, wheelPivot, 1.0f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(playerCarTexture, wheelPosition + new Vector2(55, -38), null, Color.White, playerRotation, playerPivot, 1.0f, SpriteEffects.None, 0f);
+            //spriteBatch.Draw(playerCarTexture, playerPosition, Color.White);
+            spriteBatch.DrawString(font, String.Format("Speed: {0}\nRpm: {1}\nGear: {2}\nPower(Hp): {3}\nFps: {4}", (int)playerCar.GetSpeed(), playerCar.engine.currentRPM, playerCar.Gear, (int)power, _fps), Vector2.Zero, Color.Black);
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ CarGameXNA/CarGameXNA/Scrolling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CarGameXNA
+{
+    class Background
+    {
+        public Texture2D texture;
+        public Vector2 vector;
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, vector, Color.White);
+        }
+    }
+    class Scrolling : Background
+    {
+        public Scrolling(Texture2D newTexture,Vector2 newVector)
+        {
+           texture = newTexture;
+            vector = newVector;
+        }
+        public void Update(float x)
+        {
+            vector.X += x;
+        }
+    }
+}
