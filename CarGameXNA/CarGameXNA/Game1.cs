42d8220bb5c07e680920bab1bc6269c510ec0db5
Marcus Lönneborg <marcuslonneborg@gmail.com>
2015-09-25 14:17:08 +0200

    Graphics test

--- CarGameXNA/CarGameXNA/Game1.cs
+++ CarGameXNA/CarGameXNA/Game1.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using CarGameEngine;
 
 namespace CarGameXNA
 {
@@ -16,13 +17,25 @@ namespace CarGameXNA
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        bool prevKeyGearUp, prevKeyGearDown;
+        float power = 0;
+        double powerUpdateTime=0;
+        SpriteFont font;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Car playerCar;
+        Texture2D playerCarTexture;
+        Vector2 playerPosition = new Vector2(0f, 600f);
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 900;
+            graphics.PreferredBackBufferWidth = 1600;
+            //graphics.IsFullScreen = true;
             Content.RootDirectory = "Content";
+            playerCar = new Car("Volvo", DriveTrainType.RearWheelDrive, 1400);
+
         }
 
         /// <summary>
@@ -46,7 +59,8 @@ namespace CarGameXNA
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            playerCarTexture = Content.Load<Texture2D>("bilXNA");
+            font = Content.Load<SpriteFont>("XNAFont");
             // TODO: use this.Content to load your game content here
         }
 
@@ -69,8 +83,56 @@ namespace CarGameXNA
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+            if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.Q))
+            {
+                if (!prevKeyGearUp)
+                {
+                    playerCar.GearUp();
+                    prevKeyGearUp = true;
+                }
 
-            // TODO: Add your update logic here
+            }
+            else prevKeyGearUp = false;
+            if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.A))
+            {
+                if (!prevKeyGearDown)
+                {
+                    playerCar.GearDown();
+                    prevKeyGearDown = true;
+                }
+
+            }
+            else prevKeyGearDown = false;
+
+
+            if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.Up))
+                playerCar.engine.throttlePosition += 5;
+            else
+                playerCar.engine.throttlePosition -= 5;
+
+
+            if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.Down))
+                playerCar.brakePosition += 5;
+            else
+                playerCar.brakePosition -= 5;
+
+
+            if (Keyboard.GetState(PlayerIndex.One).GetPressedKeys().Contains<Keys>(Keys.Escape))
+                this.Exit();
+
+
+
+            playerCar.Calculate(gameTime.ElapsedGameTime.TotalMilliseconds);
+            playerPosition.X += (float)playerCar.GetSpeed() / 10f;
+
+            if (playerPosition.X > GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - playerCarTexture.Width)
+                playerPosition.X = 0;
+            if (gameTime.TotalGameTime.TotalSeconds - powerUpdateTime > 0.1d)
+            {
+                powerUpdateTime = gameTime.TotalGameTime.TotalSeconds;
+                power = (power + playerCar.engine.GetPowerHP()) / 2f;
+            }
+                // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
@@ -84,6 +146,10 @@ namespace CarGameXNA
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(playerCarTexture, playerPosition, Color.White);
+            spriteBatch.DrawString(font, String.Format("Speed: {0}\nRpm: {1}\nGear: {2}\nPower(Hp): {3}", playerCar.GetSpeed(),playerCar.engine.currentRPM, playerCar.Gear,(int)power), Vector2.Zero, Color.Black);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
